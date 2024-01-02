dbdb7510374794def65ea9b716e939f13abee3cf
Ivan Beltrame <87547424+Imperatore1003@users.noreply.github.com>
2024-01-02 18:04:48 +0100

    Handling the fetch event in the service worker

--- sw.js
+++ sw.js
@@ -3,7 +3,12 @@ self.addEventListener("install", evt => {
     console.log("Service worker has been installed")
 });
 
-// Activate service worker
+// Activate service worker event
 self.addEventListener("activate", evt => {
     console.log("Service worker has been activated")
+});
+
+// Fetch to service worker event
+self.addEventListener("fetch", evt => {
+    console.log("Fetch event", evt);
 });
