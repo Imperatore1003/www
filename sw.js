dc4b4e90c23b472ae6d7e60fb628d1aa559904e4
Ivan Beltrame <87547424+Imperatore1003@users.noreply.github.com>
2024-01-02 19:22:52 +0100

    Adding offline mode with cache using the service worker

--- sw.js
+++ sw.js
@@ -1,14 +1,110 @@
+const statiCacheName = "site-static-v1";
+const pages = [
+    "/",
+    "/404",
+    "/certificati",
+    "/contatti",
+    "/privacy-policy"
+];
+const pagesFullURL = [
+    "/",
+    "/404.html",
+    "/certificati.html",
+    "/contatti.html",
+    "/privacy-policy.html"
+];
+const jsS = [
+    "/js/app.js"
+];
+const cssS = [
+    "/style.css"
+];
+const imgs = [
+    "/img/404.jpg",
+    "/img/favicon.png",
+    "/img/icon.jpg",
+    "/img/touch/icon48.png",
+    "/img/touch/icon72.png",
+    "/img/touch/icon96.png",
+    "/img/touch/icon144.png",
+    "/img/touch/icon168.png",
+    "/img/touch/icon192.png",
+    "/img/touch/icon512.png",
+    "/img/certs/grasshooper/animations.png",
+    "/img/certs/grasshooper/animations2.png",
+    "/img/certs/grasshooper/array-methods.png",
+    "/img/certs/grasshooper/code-editor.jpg",
+    "/img/certs/grasshooper/debugging.png",
+    "/img/certs/grasshooper/fundamentals.png",
+    "/img/certs/grasshooper/fundamentals2.png",
+    "/img/certs/grasshooper/interviewing.png",
+    "/img/certs/hackerx/android.jpg",
+    "/img/certs/hackerx/card-frauds.jpg",
+    "/img/certs/hackerx/database.jpg",
+    "/img/certs/hackerx/ethical-hacking.jpg",
+    "/img/certs/hackerx/gathering-info.jpg",
+    "/img/certs/hackerx/hiding-msg.jpg",
+    "/img/certs/hackerx/keyboard-spying.jpg",
+    "/img/certs/hackerx/mac.jpg",
+    "/img/certs/hackerx/network-spying.jpg",
+    "/img/certs/hackerx/os.jpg",
+    "/img/certs/hackerx/password-hacking.jpg",
+    "/img/certs/hackerx/safe-data.jpg",
+    "/img/certs/hackerx/social-hacking.jpg",
+    "/img/certs/hackerx/surfing-anonymously.jpg",
+    "/img/certs/hackerx/vulnerability.jpg",
+    "/img/certs/hackerx/website-down.jpg",
+    "/img/certs/hackerx/wep-hacking.jpg",
+    "/img/certs/hackerx/wordpress-scanning.jpg",
+    "/img/certs/hackerx/wpa2-hacking.jpg",
+    "/img/certs/hackerx/xss.jpg",
+    "/img/certs/harvard/cert-x.png",
+    "/img/certs/harvard/pro-cert-py.png",
+    "/img/certs/harvard/ver-cert-py.png",
+    "/img/certs/harvard/ver-cert-x.jpg",
+    "/img/certs/sololearn/c.jpg",
+    "/img/certs/sololearn/html.jpg",
+    "/img/certs/sololearn/java.png",
+    "/img/certs/sololearn/javascript.png",
+    "/img/certs/sololearn/jquery.jpg",
+    "/img/certs/sololearn/php.jpg",
+    "/img/certs/sololearn/python.png",
+    "/img/certs/sololearn/responsive-web-design.jpg",
+    "/img/certs/sololearn/sql.png"
+];
+const thirdParty = [
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+    "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+];
+
 // Install service worker
 self.addEventListener("install", evt => {
-    console.log("Service worker has been installed")
+    console.log("Service worker has been installed");
+    evt.waitUntil(
+        caches.open(statiCacheName).then(cache => {
+            console.log("Caching shell assets");
+            cache.addAll(pages);
+            cache.addAll(pagesFullURL);
+            cache.addAll(jsS);
+            cache.addAll(cssS);
+            cache.addAll(imgs);
+            cache.addAll(thirdParty);
+        })
+    );
 });
 
 // Activate service worker event
 self.addEventListener("activate", evt => {
-    console.log("Service worker has been activated")
+    console.log("Service worker has been activated");
 });
 
 // Fetch to service worker event
 self.addEventListener("fetch", evt => {
     console.log("Fetch event", evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
 });
