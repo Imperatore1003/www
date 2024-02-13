8a0b38e64f038398d7e0c4a9d4db73e0d97eb8de
Ivan Beltrame <87547424+Imperatore1003@users.noreply.github.com>
2024-02-13 21:50:07 +0100

    Making the experience years counter dynamic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 let month = new Date().getMonth() +1;
 let day = new Date().getDate();
 
+// Check if it is my birthday
 if (month === 1 && day === 14) {
     document.getElementById("event-header").innerHTML = '<i class="bi bi-cake2"></i>';
 }
+
+// Calculate my experience years
+document.getElementById("experience-years").innerHTML = new Date().getFullYear() - 2017;
